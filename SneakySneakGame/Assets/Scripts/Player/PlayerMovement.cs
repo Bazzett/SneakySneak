95c063374e56786bf64c2650429d9685ae5b72cd
Bazzett <elias.strandberg@edu.futuregames.nu>
2023-01-19 16:37:09 +0100

    Player prefab fix, and camera look around
    Moved the player mesh to a child of a empty game object as the parent

--- SneakySneakGame/Assets/Scripts/Player/PlayerMovement.cs
+++ SneakySneakGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    [SerializeField] private float sensX;
+    [SerializeField] private float sensY;
+
+    [SerializeField] private Transform orientation;
+    
     // Start is called before the first frame update
     void Start()
     {
