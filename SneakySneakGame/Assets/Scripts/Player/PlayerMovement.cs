184e40811ca9964eaab1720571fb6cd0a0ef1757
Bazzett <elias.strandberg@edu.futuregames.nu>
2023-01-20 13:06:40 +0100

    Walk towards target
    Movement script now walks in direction of camera

--- SneakySneakGame/Assets/Scripts/Player/PlayerMovement.cs
+++ SneakySneakGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,9 +6,12 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 
-    [SerializeField] private float walkSpeed = 10f;
+    [SerializeField] private float walkSpeed;
 
+    [SerializeField] private Transform playerBody;
+    [SerializeField] private GameObject mainCam;
     private Vector2 _moveInput;
+    private Vector3 _targetVector;
     private Rigidbody _rb;
     
 
@@ -17,21 +20,27 @@ public class PlayerMovement : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
         
     }
-
-
+    
     void Update()
     {
-        Move();
-    }
-
-    private void Move()
-    {
-        Vector3 playerVeclocity = new Vector3(_moveInput.x * walkSpeed, _rb.velocity.y, _moveInput.y * walkSpeed);
-        _rb.velocity = transform.TransformDirection(playerVeclocity);
+        Move(_targetVector);
     }
 
     private void OnMove(InputValue value)
     {
         _moveInput = value.Get<Vector2>();
+        
+        _targetVector = new Vector3(_moveInput.x, 0, _moveInput.y);
+    }
+    
+    
+    private void Move(Vector3 direction)
+    {
+        var speed = walkSpeed * Time.fixedDeltaTime;
+        direction = Quaternion.Euler(0, mainCam.gameObject.transform.eulerAngles.y, 0) * direction;
+        Vector3 norm = direction.normalized;
+        direction = (direction.magnitude > norm.magnitude) ? norm : direction;
+        
+        _rb.velocity = new Vector3(direction.x * speed, _rb.velocity.y, direction.z * speed);
     }
 }
